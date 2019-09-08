e20ca0f36488b66a660cab316c5f9a2a607f84be
Josh Cox <josh@webhosting.coop>
2019-09-08 12:34:34 -0500

    gist_url

--- TryOutRook2/background.sh
+++ TryOutRook2/background.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+GIST_URL='https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/bebd4f509596df910f5ad052ce0c870901019090/gistfile1.txt'
 launch.sh
 date '+%s' > /tmp/launch.sh
 echo 'done' >> /tmp/launch.sh
 kubectl cluster-info
-curl -s https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/fc8c9f7eebc863012c4d93960e1d5a146929704c/gistfile1.txt -o bootstrap
+curl -s $GIST_URL -o bootstrap
 bash bootstrap
