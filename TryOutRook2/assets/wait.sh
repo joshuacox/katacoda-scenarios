23bf9f6ca02fe1eee400cd9f922659fc7f9b95e8
Josh Cox <josh@webhosting.coop>
2019-09-08 12:33:21 -0500

    progress

---
+++ TryOutRook2/assets/wait.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+wait_on_pod () {
+  what_to_wait_for=$1
+  echo -n "Wait on $what_to_wait_for"
+  while true; do
+    sudo grep -i "done" $what_to_wait_for  &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+}
+
+show_progress()
+{
+  echo -n "Starting"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do
+    sudo grep -i "done" /tmp/launch.sh &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Started"
+  echo -n "Cloning"
+  while true; do
+    sudo grep -i "done" /tmp/rk_clone &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Cloned"
+  echo "Start Step 1"
+  wait_on_pod /tmp/rook-cephrook-ceph-operator
+  wait_on_pod /tmp/rook-cephrook-ceph-agent
+  wait_on_pod /tmp/rook-cephrook-ceph-mds
+  wait_on_pod /tmp/rook-cephrook-ceph-mgr
+  wait_on_pod /tmp/rook-cephrook-ceph-mon
+  wait_on_pod /tmp/rook-cephrook-ceph-osd
+  wait_on_pod /tmp/rook-cephrook-ceph-tools
+  wait_on_pod /tmp/rook-cephrook-discover
+  wait_on_pod /tmp/csi-cephfsplugin-provisioner
+  wait_on_pod /tmp/csi-rbdplugin-provisioner
+  printf "    \b\b\b\b"
+  echo ""
+  while true; do
+    sudo grep -i "done" /tmp/rk_step1 &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Step 1 finished"
+  echo -n "Start Step 2"
+  while true; do
+    sudo grep -i "done" /tmp/rk_step2 &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Step 2 finished"
+}
+
+show_progress
--- TryOutRook2/background.sh
+++ TryOutRook2/background.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 launch.sh
+date '+%s' > /tmp/launch.sh
+echo 'done' >> /tmp/launch.sh
 kubectl cluster-info
 curl -s https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/fc8c9f7eebc863012c4d93960e1d5a146929704c/gistfile1.txt -o bootstrap
+bash bootstrap
