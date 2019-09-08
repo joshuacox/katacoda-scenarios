21babf115f2d54bb2c9d74646c870e5f8deb969a
Josh Cox <josh@webhosting.coop>
2019-09-08 12:56:38 -0500

    New URL

--- TryOutRook/background.sh
+++ TryOutRook/background.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-GIST_URL='https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/bebd4f509596df910f5ad052ce0c870901019090/gistfile1.txt'
+GIST_URL='https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/c023af969195ebaa3b60bbf128b03f4b1ed3a822/gistfile1.txt'
 launch.sh
 date '+%s' > /tmp/launch.sh
 echo 'done' >> /tmp/launch.sh
