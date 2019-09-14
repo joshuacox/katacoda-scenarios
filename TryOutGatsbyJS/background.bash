44dfd7891789d7a463de926a1a0f3dfb765ce246
Josh Cox <josh@webhosting.coop>
2019-09-14 17:52:58 -0500

    gitconfig

--- TryOutGatsbyJS/background.bash
+++ TryOutGatsbyJS/background.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 . /usr/local/bin/set-env.bash
+git config --global user.email "testy@mctesterson.com"
+git config --global user.name "Testy McTesterson"
 echo 'done' >> /tmp/launch.sh
 sleep 1
