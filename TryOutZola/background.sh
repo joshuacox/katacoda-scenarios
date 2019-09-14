502d6a163c0523d59e4058eace6f80be5deb9b41
Josh Cox <josh@webhosting.coop>
2019-09-14 10:43:14 -0500

    cleanup

--- TryOutZola/background.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ~/set-env.sh
-echo 'done' >> /tmp/launch.sh
-sleep 1
-git clone --depth=1 https://github.com/getzola/zola.git
-echo 'done' >> /tmp/cloned.done
-cd zola
-docker build -t getzola/zola-test .
-echo 'done' >> /tmp/docker.built
--- TryOutZola/foreground.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. ~/set-env.sh
-~/wait.sh
