c0c4c2ea1cdfb5ed799dffaa271695f7c6d6eb6f
Josh Cox <josh@webhosting.coop>
2019-09-15 13:30:19 -0500

    waiter

--- TryOutRook-Ceph/background.sh
+++ TryOutRook-Ceph/background.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-sleep 3
+sleep 1
+wait_for_exist () {
+  countzero=0
+  while (true) {
+    if [[ -e  $1 ]]; then
+      break
+    elif [[ $counzero > 30 ]]; then
+      echo "$1 has not been created, investigate"
+    fi
+    sleep 1
+    ((++countzero))
+  }
+}
+wait_for_exist /usr/local/bin/set-env.sh
 source /usr/local/bin/set-env.sh
 GIST_URL='https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/bac4db5727c4c5ef498475e4fc22908126020c2b/gistfile1.txt'
 launch.sh
--- TryOutRook/foreground.sh
+++ TryOutRook/foreground.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-sleep 3
+sleep 1
+wait_for_exist () {
+  countzero=0
+  while (true) {
+    if [[ -e  $1 ]]; then
+      break
+    elif [[ $counzero > 30 ]]; then
+      echo "$1 has not been created, investigate"
+    fi
+    sleep 1
+    ((++countzero))
+  }
+}
+wait_for_exist /usr/local/bin/set-env.sh
 source /usr/local/bin/set-env.sh
-bash /usr/local/bin/wait.sh
+wait_for_exist /usr/local/bin/waiter
+bash /usr/local/bin/waiter
