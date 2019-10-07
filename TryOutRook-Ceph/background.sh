69a26aa727e016864b788a490f15ebb1f614ee59
Josh Cox <josh@webhosting.coop>
2019-10-07 11:43:21 -0500

    gistUrl

--- TryOutRook-Ceph/background.sh
+++ TryOutRook-Ceph/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GIST_URL='https://gist.github.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/9930d1e7329ebf4a5ef9c6d4009e20ce302dae29/gistfile1.txt'
+GIST_URL='https://gist.githubusercontent.com/joshuacox/30c0b0871622b233195a8bb5229ee5e4/raw/9930d1e7329ebf4a5ef9c6d4009e20ce302dae29/gistfile1.txt'
 sleep 1
 wait_for_exist () {
   countzero=0
@@ -22,5 +22,5 @@ date '+%s' > /tmp/launch.sh
 echo 'done' >> /tmp/launch.sh
 sleep 1
 kubectl cluster-info
-curl -s $GIST_URL -o bootstrap
+curl -sL $GIST_URL -o bootstrap
 bash bootstrap 1>/tmp/bootstrap.log 2/tmp/bootstrap.err
