f13ab07fa2397a3bbceaa6abe95d059a6cd043fe
Josh Cox <josh@webhosting.coop>
2019-10-21 10:15:14 -0500

    nvm

--- TryOutGatsbyJS-Starters/foreground.bash
+++ TryOutGatsbyJS-Starters/foreground.bash
@@ -2,3 +2,6 @@
 sleep 1
 . /usr/local/bin/set-env.bash
 /usr/local/bin/waiter
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
